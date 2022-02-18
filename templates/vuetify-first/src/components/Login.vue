2aaa44abec72c74d6f41ffa27c32206de2751300
ssakoda-wiccidu <ssakoda@wiccidu.jp>
2022-02-18 21:23:45 +0900

    vuetify

---
+++ templates/vuetify-first/src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="100"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to login page!</h1>
+      </v-col>
+    </v-row>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">ログイン</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            prepend-icon="mdi-account-circle"
+            label="ユーザ名"
+            v-model="name"
+          />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            label="パスワード"
+            v-model="password"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="info">ログイン</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    showPassword: false,
+    name: "",
+    password: "",
+  }),
+};
+</script>
