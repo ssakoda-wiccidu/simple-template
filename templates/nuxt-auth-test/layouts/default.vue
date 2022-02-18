bbd50ff1be154b7e599a11b39c2199bf6ee55082
ssakoda-wiccidu <ssakoda@wiccidu.jp>
2022-02-18 19:44:36 +0900

    nuxt-auth-test

---
+++ templates/nuxt-auth-test/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+<v-app>
+  <v-toolbar dark fixed app>
+    <v-toolbar-title>Nuxt Oauth2 Auth</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text to="/mypage">マイページ</v-btn>
+      <v-btn text @click="logout">ログアウト</v-btn>
+      <v-btn text to="/login">ログイン</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+  <v-content>
+    <v-container fluid fill-height>
+      <nuxt/>
+    </v-container>
+  </v-content>
+</v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$auth.logout();
+    }
+  }
+};
+</script>
